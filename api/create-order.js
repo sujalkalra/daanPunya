9b75e7f124e5f1cf1c56db7c4e43516549e67167
Sujal Kalra <114348922+sujalkalra@users.noreply.github.com>
2025-07-14 22:09:55 +0530

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -14,8 +14,9 @@ export default async function handler(req, res) {
   try {
     const razorpay = new Razorpay({
       key_id: process.env.RAZORPAY_KEY_ID,
-      key_secret: process.env.RAZORPAY_KEY_SECRET,
+      key_secret: process.env.RAZORPAY_KEY_SECRET
     });
+
 
     const options = {
       amount: parseInt(amount) * 100, // in paise
