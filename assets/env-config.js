811feaec6a3621c2d4d77ef45d51418229d8fe1b
sujal <kalrasujal322@gmail.com>
2025-07-14 15:41:40 +0530

    first commit

---
+++ assets/env-config.js
@@ -0,0 +1,4 @@
+window.ENV = {
+  RAZORPAY_KEY: "rzp_test_bRJREeqLsLPwZK",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby3ucRaOGrOOiUqdKwKXku3ec-JsMJw-QlITmXlHx-5l36xGk9e-zY4I2YKVpmyMLMh/exec"
+};
